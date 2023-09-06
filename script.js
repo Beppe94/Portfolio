2f34caf392b6ed4a25638979262d8ffe65bc0fc8
Beppe <giuseppe.monorchio94@hotmail.it>
2023-09-06 20:13:32 +0200

    add short about me

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('nav ol li');
+    const navLinks = document.querySelectorAll("nav ol li");
 
     navLinks.forEach((link, i) => {
         setTimeout(() => {
             link.style.opacity = '1';
-        }, 400 * (i + 1));
+        }, 500 * (i + 1));
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    const name = document.querySelectorAll("p");
+
+    name.forEach((letter, i) => {
+        setTimeout(() => {
+            letter.style.left = '0px';
+        }, 700 * (i + 1));
     })
 })
